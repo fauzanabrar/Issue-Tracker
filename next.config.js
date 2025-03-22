5ec692f5a803d766df5f834efac7298381f8883a
fauzan.abrar24 <fauzan.abrar24@gmail.com>
2025-03-22 23:41:15 +0000

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    allowedDevOrigins: [
+        '3000-idx-issuetracker-1742660322216.cluster-qpa6grkipzc64wfjrbr3hsdma2.cloudworkstations.dev'
+    ]
+}
 
 module.exports = nextConfig
