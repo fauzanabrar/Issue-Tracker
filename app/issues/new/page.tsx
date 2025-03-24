b634e44e24e8cbbaedaf9c26b094e5ecfa4138d2
fauzan.abrar24 <fauzan.abrar24@gmail.com>
2025-03-24 08:58:07 +0000

    Handle Error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,25 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import {
+  Button,
+  Callout,
+  CalloutRoot,
+  TextArea,
+  TextField,
+} from "@radix-ui/themes";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import SimpleMdeReact from "react-simplemde-editor";
+import { useState } from "react";
 
 const MdeEditor = dynamic(
   () => import("react-simplemde-editor").then((mod) => mod.default),
   {
     ssr: false,
     loading: () => <TextArea placeholder="Description" className="h-72" />,
-  },
+  }
 );
 
 interface IssueForm {
@@ -23,31 +30,43 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="space-y-5 max-w-xl"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <MdeEditor placeholder="Description" {...field} />
-          // <SimpleMdeReact placeholder="description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-5"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <MdeEditor placeholder="Description" {...field} />
+            // <SimpleMdeReact placeholder="description" {...field} />
+          )}
+        />
 
-      {/* <MdeEditor placeholder="Description" /> */}
+        {/* <MdeEditor placeholder="Description" /> */}
 
-      <Button>New Issue</Button>
-    </form>
+        <Button>New Issue</Button>
+      </form>
+    </div>
   );
 };
 
